4edb6ab43b18c91d2c33e072c41b59d7bec750af
hkwonx12 <helenkwonx2@gmail.com>
2023-10-19 12:44:09 -0400

    Main Page, Nav, User, Person started

---
+++ ghi/src/MainPage.js
@@ -0,0 +1,14 @@
+function MainPage() {
+  return (
+    <div className="MainPage">
+      <h1>Traffic Stop</h1>
+      <p>
+        {" "}
+        Help stop human trafficking by entering your missing loved one's
+        information and signing up for alerts.
+      </p>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ ghi/src/Nav.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav>
+      <div>
+        <NavLink to="/">MainPage</NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
