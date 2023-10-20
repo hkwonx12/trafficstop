bd77ce4d39da7550165178ba998eba9301917b06
Wanda McCrae <wanda.mccrae@gmail.com>
2023-10-20 18:43:04 -0400

    django apps
    Created and registered apps for missing people, reporters, and sightings. Updated requirements.txt and added .gitignore.

--- trafficstop/trafficstop/settings.py
+++ trafficstop/trafficstop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reporters.apps.ReportersConfig',
+    'sightings.apps.SightingsConfig',
+    'missing.apps.MissingConfig',
 ]
 
 MIDDLEWARE = [
