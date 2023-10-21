9ebc785c3ee6fd055ce86e790859dd40ea002e02
hkwonx12 <helenkwonx2@gmail.com>
2023-10-21 04:18:52 -0400

    Installed Leaflet for mapping on Details Page

--- ghi/src/Person/Details.js
+++ ghi/src/Person/Details.js
@@ -1,33 +1,58 @@
 import { Link } from "react-router-dom";
 import SightingDetails from "./SightingDetails";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "./styles.css";
 
 function Details() {
   return (
-    <div className="container">
-      <ul>
-        <li>Name</li>
-        <li>Height</li>
-        <li>Weight</li>
-        <li>Birth Date</li>
-        <li>Last Seen</li>
-        <li>Age</li>
-        <li>Days Missing</li>
-      </ul>
-      <div className="">
-        <button type="button" className="btn btn-warning">
-          <Link to="/profile/sighting">Add Sighting</Link>
-        </button>
-        <button type="button" className="btn btn-success">
-          <Link to="">Edit</Link>
-        </button>
-        <button type="button" className="btn btn-danger">
-          <Link to="">Delete</Link>
-        </button>
+    <>
+      <div className="details-page">
+        <div className="details">
+          <ul>
+            <li>Name</li>
+            <li>Height</li>
+            <li>Weight</li>
+            <li>Birth Date</li>
+            <li>Last Seen</li>
+            <li>Age</li>
+            <li>Days Missing</li>
+          </ul>
+        </div>
+        <div className="button">
+          <button type="button" className="btn btn-warning">
+            <Link to="/profile/sighting">Add Sighting</Link>
+          </button>
+          <button type="button" className="btn btn-success">
+            <Link to="">Edit</Link>
+          </button>
+          <button type="button" className="btn btn-danger">
+            <Link to="">Delete</Link>
+          </button>
+        </div>
+
+        <div className="map-wrap">
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                Sighting 1 <br /> 10/20/2023
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+        <div className="Details-table">
+          <SightingDetails />
+        </div>
       </div>
-      <div className="container">
-        <SightingDetails />
-      </div>
-    </div>
+    </>
   );
 }
 
