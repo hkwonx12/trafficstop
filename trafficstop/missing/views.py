58f01d83ffd76700a2f3bbad2e7e961cc4faef39
Wanda McCrae <wanda.mccrae@gmail.com>
2023-10-21 02:02:49 -0400

    Read, update, and delete a specific missing person.

--- trafficstop/missing/views.py
+++ trafficstop/missing/views.py
@@ -7,7 +7,7 @@ from .encoders import MissingPersonEncoder
 from reporters.models import Reporter
 
 
-@require_http_methods(["GET", "POST"])
+@require_http_methods(["POST", "GET"])
 def api_missingperson(request):
     """
     POST: creates a new missing person
@@ -16,10 +16,9 @@ def api_missingperson(request):
     if request.method == "POST":
         new_person = json.loads(request.body)
 
-        reporter = Reporter.objects.get(id=new_person["reporter_id"])
-        new_person["reporter"] = reporter
-
         try:
+            reporter = Reporter.objects.get(id=new_person["reporter_id"])
+            new_person["reporter"] = reporter
             person = MissingPerson.objects.create(**new_person)
             return JsonResponse(
                 person,
@@ -37,3 +36,50 @@ def api_missingperson(request):
             encoder=MissingPersonEncoder,
             safe=False,
         )
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_persondetails(request, id):
+    """
+    GET: returns the missing person object
+    PUT: updates the missing person object
+    DELETE: deletes the missing person object
+    """
+    if request.method == "GET":
+        try:
+            person = MissingPerson.objects.get(id=id)
+            return JsonResponse(
+                person,
+                encoder=MissingPersonEncoder,
+                safe=False,
+            )
+        except MissingPerson.DoesNotExist:
+            return JsonResponse(
+                {"ERROR": f"No person with ID {id} exists"},
+                status=404
+            )
+    elif request.method == "PUT":
+        content = json.loads(request.body)
+        MissingPerson.objects.filter(id=id).update(**content)
+        try:
+            person = MissingPerson.objects.get(id=id)
+            return JsonResponse(
+                person,
+                encoder=MissingPersonEncoder,
+                safe=False
+            )
+        except MissingPerson.DoesNotExist:
+            return JsonResponse(
+                {"ERROR": f"No person with ID {id} exists"},
+                status=404
+            )
+    else:
+        # DELETE
+        try:
+            count, _ = MissingPerson.objects.filter(id=id).delete()
+            return JsonResponse({"delete": count > 0})
+        except MissingPerson.DoesNotExist:
+            return JsonResponse(
+                {"ERROR": "Person does not exist"},
+                status=404
+            )
